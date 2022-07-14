aad363c190def4ac66a8e8cd1d56b0f33da767de
Rosset <theo.rosset@outlook.fr>
2022-07-14 16:37:00 +0200

    feat(factories): add model for article card photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,35 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, tagline, price, city, country, id } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    //create element
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const countryCity = document.createElement("p");
+    const tagLine = document.createElement("p");
+    const pricePhotographer = document.createElement("p");
+    const h2 = document.createElement("h2");
+    //set attribute or class
+    article.setAttribute("data-id", id);
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", `Photo de profil de ${name}`);
+    countryCity.classList.add("countryCityPhotographer");
+    tagLine.classList.add("tagLinePhotographer");
+    pricePhotographer.classList.add("pricePhotographer");
+    //texte of element
+    h2.textContent = name;
+    countryCity.innerText = `${city}, ${country}`;
+    tagLine.innerText = `${tagline}`;
+    pricePhotographer.innerText = `${price} €/jours`;
+    //appendchild element
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(countryCity);
+    article.appendChild(tagLine);
+    article.appendChild(pricePhotographer);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
