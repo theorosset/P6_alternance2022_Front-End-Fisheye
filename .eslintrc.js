f7aab298c8ac97bc035d459c521462027ecc7f86
Rosset <theo.rosset@outlook.fr>
2022-07-29 10:22:19 +0200

    refacto(service): add new call and reorganize

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+//eslint-disable-next-line
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    },
+        "indent": ["error", 2]
+};
--- scripts/service/service.js
+++ scripts/service/service.js
@@ -1,44 +1,26 @@
 let dataJson = "../../data/photographers.json";
 
 export async function fetchPhotographers() {
-    return fetch(dataJson)
-      .then((res) => {
-        return res.json();
-      })
-      .catch((err) => {
-        return err;
-      }); 
-  }
-
-export async function fetchPhotographerById(idPhotographer){
-    return fetch(dataJson)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        return data.photographers.find(({ id }) => {
-          return id === parseInt(idPhotographer);
-        });
-      })
-      .catch((err) => {
-        return err;
-      });
+  return fetch(dataJson)
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      return err;
+    });
 }
 
-export async function fetchMediaPhotographerById(id){
-    return fetch(dataJson)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-         data.media.forEach((media)=> { 
-          if(media.photographerId === parseInt(id)){
-            mediaFactory(media).getMediaDom()
-          }
-         })
-        
-      })
-      .catch((err) => {
-        return err;
+export async function fetchPhotographerById(idPhotographer) {
+  return fetch(dataJson)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      return data.photographers.find(({ id }) => {
+        return id === parseInt(idPhotographer);
       });
+    })
+    .catch((err) => {
+      return err;
+    });
 }
